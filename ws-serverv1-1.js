b0224d78f7948b0cf0728d6ee3fa4fff56e482d2
ilam0602 <isaac.lam@live.com>
2024-05-08 14:35:35 -0700

    add archive and active channel names

--- ws-serverv1-1.js
+++ ws-serverv1-1.js
@@ -1,22 +1,20 @@
 const { WebClient } = require("@slack/web-api");
 const WebSocket = require("ws");
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
-
-
+require("dotenv").config();
 
 let conversation_id = null;
+let archive_id = null;
 
 const connected_clients = new Map();
 
 const slackClient = new WebClient(process.env.SLACK_TOKEN);
-const channel_name = process.env.CHANNEL_NAME;
+const active_channel_name = process.env.ACTIVE_CHANNEL_NAME;
+const archive_channel_name = process.env.ARCHIVE_CHANNEL_NAME;
 const koreBotWebhookUrl = process.env.KORE_BOT_WEBHOOK_URL;
 const identity = process.env.IDENTITY;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-
-
 
 const bearerToken = jwt.sign(
   {
@@ -31,29 +29,38 @@ const bearerToken = jwt.sign(
 );
 
 async function fetchData(url, options) {
-    const fetch = (await import('node-fetch')).default;
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
-    }
+  const fetch = (await import("node-fetch")).default;
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
   }
-
-
-
+}
 
 // Fetch channel ID
 (async () => {
   try {
     const result = await slackClient.conversations.list();
     const channel = result.channels.find(
-      (channel) => channel.name === channel_name
+      (channel) => channel.name === active_channel_name
     );
     if (channel) {
       conversation_id = channel.id;
       console.log(`Found conversation ID: ${conversation_id}`);
+    }
+  } catch (e) {
+    console.error(`Error fetching conversations: ${e}`);
+  }
+  try {
+    const result = await slackClient.conversations.list();
+    const channel = result.channels.find(
+      (channel) => channel.name === archive_channel_name
+    );
+    if (channel) {
+      archive_id = channel.id;
+      console.log(`Found conversation ID: ${archive_id}`);
     }
   } catch (e) {
     console.error(`Error fetching conversations: ${e}`);
@@ -63,15 +70,14 @@ async function fetchData(url, options) {
 const wss = new WebSocket.Server({ port: 8765 });
 
 wss.on("connection", function connection(ws) {
-
   let ms_ts = null;
   let stored_messages = [];
   async function onConnect(ws) {
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Authorization': `Bearer ${bearerToken}`,
-        'Content-Type': 'application/json',
+        Authorization: `Bearer ${bearerToken}`,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         session: {
@@ -89,28 +95,27 @@ wss.on("connection", function connection(ws) {
         },
       }),
     };
-  
+
     try {
       const data = await fetchData(koreBotWebhookUrl, options);
       const responseMessage = data["data"][0]["val"];
       const sessionId = data["sessionId"];
-      
-        ws.send(`From Kore: ${responseMessage}`);
-  
+
+      ws.send(`From Kore: ${responseMessage}`);
+
       const result = await slackClient.chat.postMessage({
         channel: conversation_id,
         text: `Kore Session ID: ${sessionId}`,
       });
-  
+
       ms_ts = result.ts;
       console.log("res: ", responseMessage);
-  
-  
-      connected_clients.set(ms_ts,ws);
-  
-        console.log(`Slack ms_ts: ${ms_ts},${sessionId}`)
-        ws.send(`Slack ms_ts: ${ms_ts},${sessionId}`);
-  
+
+      connected_clients.set(ms_ts, ws);
+
+      console.log(`Slack ms_ts: ${ms_ts},${sessionId}`);
+      ws.send(`Slack ms_ts: ${ms_ts},${sessionId}`);
+
       await slackClient.chat.postMessage({
         channel: conversation_id,
         thread_ts: ms_ts,
@@ -134,7 +139,7 @@ wss.on("connection", function connection(ws) {
           const curr_messages = result.messages
             .filter((msg) => msg.user !== "U070GNA54LB")
             .map((msg) => msg.text);
-  
+
           if (stored_messages.length !== curr_messages.length) {
             curr_messages
               .slice(stored_messages.length)
@@ -150,69 +155,81 @@ wss.on("connection", function connection(ws) {
       }
     }
   }
-  
+
   receiveMessages();
   ws.on("message", async function incoming(message) {
     // Convert Buffer to string
     const messageText = message.toString();
     console.log("Received message: ", messageText);
-    if(messageText.substring(0,6) === "ms_ts:"){
-        ms_ts = messageText.substring(6, messageText.length);
-        connected_clients.set(ms_ts,ws);
-        try {
-            const result = await slackClient.conversations.replies({
-            channel: conversation_id,
-            ts: messageText.substring(6, messageText.length),
-            });
-            const curr_messages = result.messages
-            .map((msg) => msg.text);
-            console.log("curr_messages: ", curr_messages);
-    
-              for(let i = 0; i < curr_messages.length; i++){
-                connected_clients.get(ms_ts).send(`HISTORY: ${curr_messages[i]}`);
-              }
-              connected_clients.get(ms_ts).send(`HISTORY DONE:`);
-            const messages_to_store = result.messages
-            .filter((msg) => msg.user !== "U070GNA54LB")
-            .map((msg) => msg.text);
-    
-            messages_to_store.forEach((message) => {
-                stored_messages.push(message);
-            });
-
-            
-        } catch (e) {
-            console.error(`Error retrieving replies: ${e}`);
-        }
-
-    }else if (messageText.substring(0,11) == "new_thread:"){
-        onConnect(ws);
-    }else{
-
-    setTimeout(async () => {
+    if (messageText.substring(0, 6) === "ms_ts:") {
+      ms_ts = messageText.substring(6, messageText.length);
+      connected_clients.set(ms_ts, ws);
       try {
-        await slackClient.chat.postMessage({
+        const result = await slackClient.conversations.replies({
           channel: conversation_id,
-          thread_ts: ms_ts,
-          text: `User: ${messageText}`, // Use the converted string here
+          ts: messageText.substring(6, messageText.length),
+        });
+        const curr_messages = result.messages.map((msg) => msg.text);
+        console.log("curr_messages: ", curr_messages);
 
+        for (let i = 0; i < curr_messages.length; i++) {
+          connected_clients.get(ms_ts).send(`HISTORY: ${curr_messages[i]}`);
+        }
+        connected_clients.get(ms_ts).send(`HISTORY DONE:`);
+        const messages_to_store = result.messages
+          .filter((msg) => msg.user !== "U070GNA54LB")
+          .map((msg) => msg.text);
+
+        messages_to_store.forEach((message) => {
+          stored_messages.push(message);
         });
       } catch (e) {
-        console.error(`Error posting message: ${e}`);
+        console.error(`Error retrieving replies: ${e}`);
+      }
+    } else if (messageText.substring(0, 11) == "new_thread:") {
+      onConnect(ws);
+    } else if (messageText == "archive:") {
+      console.log("closing ticket now: ", ms_ts);
+      const result = await slackClient.conversations.replies({
+        channel: conversation_id,
+        ts: ms_ts,
+      });
+      const curr_messages = result.messages;
+      let ts_delete = [];
+      for (let i = 0; i < curr_messages.length; i++) {
+        ts_delete.push(curr_messages[i].ts);
       }
 
-    }, 500);
+      //delete messages on slack channel
+
+      // for (let i = ts_delete.length-1; i >=0; i--) {
+      //   await slackClient.chat.delete({
+      //     channel: conversation_id,
+      //     ts: ts_delete[i],
+      //   });
+      // }
+
+    } else {
+      setTimeout(async () => {
+        try {
+          await slackClient.chat.postMessage({
+            channel: conversation_id,
+            thread_ts: ms_ts,
+            text: `User: ${messageText}`, // Use the converted string here
+          });
+        } catch (e) {
+          console.error(`Error posting message: ${e}`);
+        }
+      }, 500);
     }
   });
 
   ws.on("close", function close() {
-    console.log(connected_clients)
-    console.log("Client disconnected");
+    // console.log(connected_clients)
+    // console.log("Client disconnected");
     connected_clients.delete(ms_ts);
-    console.log(connected_clients)
-        ms_ts = null;
-        stored_messages = [];
+    // console.log(connected_clients);
+    ms_ts = null;
+    stored_messages = [];
   });
 });
-
-
