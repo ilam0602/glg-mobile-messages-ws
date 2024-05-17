006f9fdb26cdcdcca879c0a30167fb021445e10f
ilam0602 <isaac.lam@live.com>
2024-05-17 14:38:02 -0700

    add functionality to send message to kore and receive response

--- ws-serverv1-1.js
+++ ws-serverv1-1.js
@@ -7,6 +7,7 @@ let conversation_id = null;
 let archive_id = null;
 
 const connected_clients = new Map();
+const client_to_kore = new Map();
 
 const slackClient = new WebClient(process.env.SLACK_TOKEN);
 const active_channel_name = process.env.ACTIVE_CHANNEL_NAME;
@@ -16,17 +17,7 @@ const identity = process.env.IDENTITY;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
-const bearerToken = jwt.sign(
-  {
-    sub: identity,
-    iss: clientId,
-    appId: clientId,
-    iat: Math.floor(Date.now() / 1000) - 30, // Issued at time, 30 seconds ago
-    exp: Math.floor(Date.now() / 1000) + 60 * 60, // Expiry time, typically 1 hour
-    jti: 1234,
-  },
-  clientSecret
-);
+
 
 async function fetchData(url, options) {
   const fetch = (await import("node-fetch")).default;
@@ -72,6 +63,17 @@ const wss = new WebSocket.Server({ port: 8765 });
 wss.on("connection", function connection(ws) {
   let ms_ts = null;
   let stored_messages = [];
+  const bearerToken = jwt.sign(
+    {
+      sub: identity,
+      iss: clientId,
+      appId: clientId,
+      iat: Math.floor(Date.now() / 1000) - 30, // Issued at time, 30 seconds ago
+      exp: Math.floor(Date.now() / 1000) + 60 * 60, // Expiry time, typically 1 hour
+      jti: 1234,
+    },
+    clientSecret
+  );
   async function onConnect(ws) {
     const options = {
       method: "POST",
@@ -112,6 +114,7 @@ wss.on("connection", function connection(ws) {
       console.log("res: ", responseMessage);
 
       connected_clients.set(ms_ts, ws);
+      client_to_kore.set(ms_ts, true);
 
       console.log(`Slack ms_ts: ${ms_ts},${sessionId}`);
       ws.send(`Slack ms_ts: ${ms_ts},${sessionId}`);
@@ -127,6 +130,7 @@ wss.on("connection", function connection(ws) {
       ws.send(JSON.stringify({ error: "An error occurred." }));
     }
   }
+
   async function receiveMessages() {
     while (true) {
       await new Promise((resolve) => setTimeout(resolve, 5000));
@@ -155,10 +159,65 @@ wss.on("connection", function connection(ws) {
       }
     }
   }
+  async function sendMessageToSlack(messageText,ms_ts,conversation_id){
+    setTimeout(async () => {
+      try {
+        await slackClient.chat.postMessage({
+          channel: conversation_id,
+          thread_ts: ms_ts,
+          text: `User: ${messageText}`, // Use the converted string here
+        });
+      } catch (e) {
+        console.error(`Error posting message: ${e}`);
+      }
+    }, 500);
+  }
 
-  receiveMessages();
-  ws.on("message", async function incoming(message) {
-    // Convert Buffer to string
+  async function sendMessageToKore(message,ms_ts){
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: {
+        type: "text",
+        val: message,
+      },
+      from: {
+        id: identity,
+      },
+      to: {
+        id: "st-9553962c-19e6-5e9c-9a27-7001e994b776",
+      },
+    }),
+  };
+
+    try {
+      const data = await fetchData(koreBotWebhookUrl, options);
+      console.log(data);
+      const responseMessage = data["data"][0]["val"];
+
+      ws.send(`From Kore: ${responseMessage}`);
+      const transferMessage = 'I\'m sorry. I was unable to process your request. I will be transfering you to a live agent. One moment please.'
+      if(responseMessage == transferMessage){
+        // console.log("transferring");
+        client_to_kore.set(ms_ts, false);
+      }
+      await slackClient.chat.postMessage({
+        channel: conversation_id,
+        thread_ts: ms_ts,
+        text: `Kore Bot: ${responseMessage}`,
+      });
+    } catch (error) {
+      console.error("Error during WebSocket handling: ", error);
+      // You can send a message back to the client through WebSocket if necessary
+      ws.send(JSON.stringify({ error: "An error occurred." }));
+    }
+  }
+
+  async function sendMessage(message) {
     const messageText = message.toString();
     console.log("Received message: ", messageText);
     if (messageText.substring(0, 6) === "ms_ts:") {
@@ -208,25 +267,25 @@ wss.on("connection", function connection(ws) {
       //     ts: ts_delete[i],
       //   });
       // }
-
     } else {
-      setTimeout(async () => {
-        try {
-          await slackClient.chat.postMessage({
-            channel: conversation_id,
-            thread_ts: ms_ts,
-            text: `User: ${messageText}`, // Use the converted string here
-          });
-        } catch (e) {
-          console.error(`Error posting message: ${e}`);
-        }
-      }, 500);
+      if(client_to_kore.get(ms_ts) == false){
+        sendMessageToSlack(messageText,ms_ts,conversation_id);
+      }else{
+        sendMessageToSlack(messageText,ms_ts,conversation_id);
+        sendMessageToKore(messageText,ms_ts);
+      }
     }
+  }
+
+  receiveMessages();
+  ws.on("message", async function incoming(message) {
+    // Convert Buffer to string
+    sendMessage(message);
   });
 
   ws.on("close", function close() {
     // console.log(connected_clients)
-    // console.log("Client disconnected");
+    console.log("Client disconnected");
     connected_clients.delete(ms_ts);
     // console.log(connected_clients);
     ms_ts = null;
